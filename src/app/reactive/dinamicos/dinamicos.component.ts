724f58c3c7aa4666817df4c169f5f85a8b97b422
Huntress <quiqueze@gmail.com>
2022-09-21 01:19:52 -0500

    Se incluyen los ejemplos de formularios reactivos dinamicos

--- src/app/reactive/dinamicos/dinamicos.component.ts
+++ src/app/reactive/dinamicos/dinamicos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, FormArray, FormControl } from '@angular/forms';
 
 
 @Component({
@@ -8,12 +9,60 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DinamicosComponent implements OnInit {
 
-    constructor() {
+    miFormulario: FormGroup = this.builder.group({
+        nombre: ["", [
+            Validators.required,
+            Validators.minLength(3)
+        ]],
+        favoritos: this.builder.array([
+            ["WoW", Validators.required],
+            ["FF8", Validators.required],
+            ["Mario", Validators.required],
+        ], Validators.required)
+    });
+
+    get favoritos(): FormArray {
+        return this.miFormulario.get("favoritos") as FormArray
+    }
+
+    nuevoFavorito: FormControl = this.builder.control("", Validators.required);
+
+    constructor(
+        private builder: FormBuilder
+    ) {
         // Do nothing
     }
 
     ngOnInit(): void {
         // Do nothing
+    }
+
+    campoNoValido(campo: string) {
+        return this.miFormulario.controls[campo].errors && this.miFormulario.controls[campo].touched;
+    }
+
+    guardar() {
+        if( this.miFormulario.invalid ) {
+            this.miFormulario.markAllAsTouched();
+            return;
+        }
+
+        console.log(this.miFormulario.value);
+        this.miFormulario.reset();
+        this.favoritos.clear();
+    }
+
+    agregar() {
+        if( this.nuevoFavorito.invalid ) {
+            return;
+        }
+
+        this.favoritos.push(this.builder.control(this.nuevoFavorito.value, Validators.required));
+        this.nuevoFavorito.reset();
+    }
+
+    eliminar(index: number) {
+        this.favoritos.removeAt(index);
     }
 
 }
